84c5f2abe048d6ba9fa8f5e65a87876c99b4a485
abdalla162 <abdalla162@hotmail.com>
2024-01-25 20:10:56 +0100

    Arbeidskrav 2

--- oblig2.js
+++ oblig2.js
@@ -1,10 +1,12 @@
 let resourceHTML = "";
 
-// Funksjon for å oppdatere innholdet basert på kategori
+// Dette er da en funksjon for å da oppdatere innholdet basert på kategori
 function updateContent(category) {
     const resourceContent = document.getElementById("resourceContent");
+
     const selectedResource = resources.find(resource => resource.category === category);
 
+    //Bruke li tag for å få punkliste i urlene
     if (selectedResource) {
         const titleURL = selectedResource.sources.map(source => `
             <li><a href="${source.url}" target="_blank">${source.title}</a></li>
@@ -18,25 +20,30 @@ function updateContent(category) {
     }
 }
 
-// Funksjon for knappetrykk
-function buttonClick(category) {
-    console.log( `Button clicked for category: ${category}`);
-    updateContent(category);
-}
 
-
-// Generer HTML for ressurslisten
+// Vi generer HTML for ressurslisten
 resources.forEach((resource) => {
     resourceHTML += `
         <li><a href="#"><button onclick="buttonClick('${resource.category}')">${resource.category}</button></a></li>
     `;
 });
 
-// Sett HTML for ressurslisten
+
+// Sette HTML for ressurslisten
 const resourceList = document.getElementById("resourceList");
 resourceList.innerHTML = resourceHTML;
 
-// Vis innholdet for første kategori som standard
+
+// Lager en funksjon for knappetrykk
+function buttonClick(category) {
+    console.log( `Button clicked for category: ${category}`);
+    updateContent(category);
+}
+
+
+// Her skal den vise innholdet for første kategori som standard, altså den html.
 if (resources.length > 0) {
     updateContent(resources[0].category);
 }
+
+
